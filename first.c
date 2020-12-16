c7dcf59c99a83175bed9b5360de4530c7d49f529
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2020-12-16 11:27:11 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Enter any number");
+    scanf("%d", &num);
+    printf("Yournumber is %d", num);
+}
