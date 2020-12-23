a6777075d1d2b4ebd91e13ee16df4205de387412
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2020-12-23 20:19:01 +0530

    Add files via upload

---
+++ nestedif.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j,ch=97;
+    for(i=0;i<5;i++)
+    {
+for(j=0;j<5;j++)
+
+{
+printf(" %c", ch);
+ch++;
+}
+    }
+    return 0;
+}
