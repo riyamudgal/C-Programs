654d13bdbd3638dd55740d726d88fceef7d3894d
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2020-12-28 20:13:50 +0530

    Add files via upload

--- return_noargu.c
+++ return_noargu.c
@@ -6,7 +6,7 @@ int minimum()
 {
     int num1,num2,num3;
     printf("enter any 3 numbers");
-    scanf("%d %d", &num1 ,&num2 ,&num3);
+    scanf("%d %d %d", &num1 ,&num2 ,&num3);
     if(num1<num2)
     {
         if(num1<num3)
